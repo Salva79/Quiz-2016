dbc993fe860224d5bfb0b0a82a082e7c5125a908
Salva <salvador@dosmares.com>
2016-11-07 08:04:32 +0100

    Autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,30 +1,41 @@
 var models=require('../models/models.js');
 
-// GET /quiz/quiz/:id
-
-exports.show =function(req,res){
-	models.Quiz.findById(req.params.quizId).then(function(quiz){
-		res.render('quizes/show',{quiz: quiz});			
-	})	
-};
-
-//GET /quiz/:id/answer
-
-exports.answer = function(req,res){
-	models.Quiz.findById(req.params.quizId).then(function(quiz){
-		if(req.query.respuesta === quiz.respuesta){
-			res.render('quizes/answer',{quiz: quiz, respuesta: 'Correcto'});
+// AUTOLOAD - FACTORIZA el código si ruta incluye :quizId
+exports.load = function(req,res,next,quizId){
+	models.Quiz.findById(quizId).then(function(quiz){
+		if(quiz){
+			req.quiz = quiz;
+			next();
 		}else{
-			res.render('quizes/answer',{quiz: quiz, respuesta: 'Incorrecto'});
+			next(new Error('No existe quiz Id= ' + quizId));
 		}
-	})
+	}).catch(function(error){
+		next(error);
+	});
 };
 
-
-// GET /quiz/index
+// GET /quiz - index
 
 exports.index =function(req,res){
 	models.Quiz.findAll().then(function(quizes){
 		res.render('quizes/index',{quizes: quizes});			
 	})	
 };
+
+// GET /quiz/quiz/:id
+
+exports.show =function(req,res){
+	res.render('quizes/show',{quiz: req.quiz});				
+};
+
+//GET /quiz/:id/answer
+
+exports.answer = function(req,res){
+	var resultado = 'Incorrecto';
+	if(req.query.respuesta === req.quiz.respuesta){
+		resultado = 'Correcto';
+	}
+	res.render('quizes/answer',{quiz: req.quiz, respuesta: resultado});
+};
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var router = express.Router();
-
 var quizController=require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {res.render('index', { title: 'Quiz-2016' });});
 
+router.param('quizId', quizController.load);
+
 /* GET de quizes */
 router.get('/quizes', quizController.index);
-router.get('/quizes/:quizId(\\d+)', quizController.show)
-router.get('/quizes/:quizId(\\d+)/answer',quizController.answer)
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer',quizController.answer);
 
 module.exports = router;
