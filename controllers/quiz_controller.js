19e1029ad52fe978c18633cd024a160cb14cbefa
Salva <salvador@dosmares.com>
2016-11-15 08:07:30 +0100

    editar y borrar

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -41,10 +41,7 @@ exports.answer = function(req,res){
 //GET /quiz/new
 
 exports.new = function(req,res) {
-	var quiz = models.Quiz.build({
-		pregunta: 'Pregunta',
-		respuesta: 'Respuesta'
-	});
+	var quiz = models.Quiz.build({});
 	res.render('quizes/new', {quiz:quiz});
 }
 
@@ -60,4 +57,35 @@ exports.create = function(req,res){
 				res.redirect('/quizes')});		
 		}	
 	})
+}
+
+//GET /quiz/edit
+
+exports.edit = function(req,res) {
+	var quiz = req.quiz;
+	res.render('quizes/edit', {quiz: quiz});
+}
+
+//PUT /quizes/:id
+exports.update = function(req,res){
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+
+	req.quiz.validate().then(
+		function(err){
+			if(err){
+				res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+			}else{
+				req.quiz.save({fields: ["pregunta","respuesta"]})
+				.then(function(){res.redirect('/quizes');});			
+			}
+		}
+	)
+}
+
+//DELETE /quizes/:id
+exports.destroy = function(req, res){
+	req.quiz.destroy().then(function(){
+		res.redirect('/quizes');
+	}).catch(function(error){next(error)});
 }
